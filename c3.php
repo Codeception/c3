fa7c2290afcbdf2f8540dae8d35554286e8f4fdb
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2015-07-12 10:46:19 -0700

    Allow c3 to use codeception.dist.yml
    c3 does not check for `codeception.dist.yml` if `codeception.yml` does not exist.
    
    This patch checks for `codeception.dist.yml`.

--- c3.php
+++ c3.php
@@ -55,11 +55,15 @@ if (!class_exists('\\Codeception\\Codecept')) {
 }
 
 // Load Codeception Config
+$config_dist_file = realpath(__DIR__) . DIRECTORY_SEPARATOR . 'codeception.dist.yml';
 $config_file = realpath(__DIR__) . DIRECTORY_SEPARATOR . 'codeception.yml';
 if (isset($_SERVER['HTTP_X_CODECEPTION_CODECOVERAGE_CONFIG'])) {
     $config_file = realpath(__DIR__) . DIRECTORY_SEPARATOR . $_SERVER['HTTP_X_CODECEPTION_CODECOVERAGE_CONFIG'];
 }
-if (!file_exists($config_file)) {
+if (!file_exists($config_file) && file_exists($config_dist_file)) {
+    // Use codeception.dist.yml for configuration.
+    $config_file = $config_dist_file;
+} else {
     __c3_error(sprintf("Codeception config file '%s' not found", $config_file));
 }
 try {
