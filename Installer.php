050c83a712bc017ef219587eefe496c779826500
Davert <davert.php@resend.cc>
2015-11-25 05:22:24 +0200

    installer updated

--- Installer.php
+++ Installer.php
@@ -1,46 +1,59 @@
 <?php
 namespace Codeception\c3;
-use Composer\Package\PackageInterface;
-use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Installer\LibraryInstaller;
+use Composer\Script\ScriptEvents;
 
 class InstallerPlugin implements PluginInterface
 {
+    /**
+     * @var IOInterface
+     */
+    private $io;
+
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new Installer($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $this->io = $io;
     }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ScriptEvents::POST_INSTALL_CMD => array(
+                array('copyC3', 0)
+            ),
+            ScriptEvents::POST_UPDATE_CMD => array(
+                array('askForUpdate', 0)
+            )
+        );
+    }
+
+    public function copyC3()
+    {
+        if ($this->c3HasChanged()) {
+            $this->io->write("<info>[c3]</info> c3.php is already up-to-date");
+            return;
+        }
+
+        $this->io->write("<info>[c3]</info> Copying c3.php to the root of your project...");
+        copy(__DIR__.DIRECTORY_SEPARATOR.'c3.php', getcwd().DIRECTORY_SEPARATOR.'c3.php');
+        $this->io->write("<info>[c3]</info> Include c3.php into index.php in order to collect codecoverage from server scripts");
+    }
+
+    public function askForUpdate()
+    {
+        if (!$this->c3HasChanged()) return;
+        $replace = $this->io->askConfirmation("Do you want to replace c3.php with latest version?", false);
+        if (!$replace) return;
+        $this->copyC3();
+    }
+
+    private function c3HasChanged()
+    {
+        return file_exists(getcwd().DIRECTORY_SEPARATOR.'c3.php') &&
+            md5_file(__DIR__.DIRECTORY_SEPARATOR.'c3.php') === md5_file(getcwd().DIRECTORY_SEPARATOR.'c3.php');
+    }
+
 }
 
-class Installer extends LibraryInstaller
-{
-    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        parent::install($repo, $package);
-        $this->filesystem->copyThenRemove($this->getInstallPath($package) . DIRECTORY_SEPARATOR . 'c3.php', 'c3.php');
-    }
-
-    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
-    {
-        parent::update($repo, $initial, $target);
-        $this->filesystem->copyThenRemove($this->getInstallPath($target) . DIRECTORY_SEPARATOR . 'c3.php', 'c3.php');
-    }
-
-    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        parent::uninstall($repo, $package);
-        $this->filesystem->remove('c3.php');
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($packageType)
-    {
-        return 'codeception-c3' === $packageType;
-    }
-}
