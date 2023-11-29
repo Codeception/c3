580cbd21e2ae6be803e5652594433b6e37d2cb65
Marcel Bolten <github@marcelbolten.de>
2023-11-28 20:18:15 -0800

    30 sec maximum wait time for all tests to finish upon report request

--- c3.php
+++ c3.php
@@ -265,7 +265,7 @@ if (!defined('C3_CODECOVERAGE_MEDIATE_STORAGE')) {
                 // wait until serialized coverage data of all tests is written to file
                 $blockfilename = realpath(C3_CODECOVERAGE_MEDIATE_STORAGE) . DIRECTORY_SEPARATOR . 'block_report';
                 if (file_exists($blockfilename) && filesize($blockfilename) !== 0) {
-                    $retries = 10;
+                    $retries = 120; // 30 sec total
                     while (file_get_contents($blockfilename) !== '0' && --$retries >= 0) {
                         usleep(250000); // 0.25 sec
                     }
